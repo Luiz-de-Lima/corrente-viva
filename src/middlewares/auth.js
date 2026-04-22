99435e204487c93098d4e17741cf10f5bbbaefb8
luiz de Lima <luiz12359@gmail.com>
2026-04-22 15:05:44 -0300

    feat: adicionado middleware de autenticação JWT

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const autenticar = (req, res, next) => {
+  const token = req.header.authorization;
+
+  if (!token) {
+    return res.status(401).json({ mensagem: "Token não informado" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.responsavel = decoded;
+    next();
+  } catch (error) {
+    console.log("erro");
+    res.status(401).json({ mensagem: "Token inválido" });
+  }
+};
+module.exports = { autenticar };
